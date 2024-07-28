a6e1634cb4a34bdcfc84d0a19a8825d3bd82251f
AmanMayank <aman.msingh87@gmail.com>
2024-07-28 18:00:00 +0530

    hooks added

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,49 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+
 const login = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="w-full h-full flex justify-center items-center page-gradient">
       <div className="bg-gradient w-[648px] border-[1px] rounded-2xl h-auto flex-row items-center justify-center text-center p-[60px]">
         <p className="font-barlow font-semibold text-5xl loading-[58px] mb-[32px] text-customBlack">
           Welcome to <span className="text-customPurple">Workflo</span>!
         </p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             className="w-[528px] mx-auto block  mb-[24px] px-3 py-4 rounded-lg font-inter text-customGrey bg-bgInput"
             type="email"
             placeholder="jgardner@gmail.com"
+            onChange={handleOnChange}
+            name="email"
+            value={data.email}
           ></input>
           <input
             className="w-[528px] mx-auto block  mb-[22px] px-3 py-4 rounded-lg text-customGrey bg-bgInput"
             type="password"
             placeholder="*************"
+            onChange={handleOnChange}
+            name="password"
+            value={data.password}
           ></input>
 
           <button className="font-inter w-[528px] border-2 py-[14px] text-2xl leading-6 btn-gradient rounded-lg">
@@ -24,7 +53,9 @@ const login = () => {
 
         <p className="mt-8 font-inter text-2xl text-customGrey">
           Don’t have an account? Create a{" "}
-          <span className="text-customBlue">new account</span>
+          <Link href="/signup">
+            <span className="text-customBlue">new account</span>
+          </Link>
         </p>
       </div>
     </div>
--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -1,25 +1,61 @@
-const signUp = () => {
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+
+const signup = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  console.log(data);
+
   return (
     <div className="w-full h-full flex justify-center items-center page-gradient">
       <div className="bg-gradient w-[648px] border-[1px] rounded-2xl h-auto flex-row items-center justify-center text-center p-[60px]">
         <p className="font-barlow font-semibold text-5xl loading-[58px] mb-[32px] text-customBlack">
           Welcome to <span className="text-customPurple">Workflo</span>!
         </p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             className="w-[528px] mx-auto block  mb-[24px] px-3 py-4 rounded-lg font-inter text-customGrey bg-bgInput"
             type="text"
+            onChange={handleOnChange}
             placeholder="Joe Gardner"
+            name="name"
+            value={data.name}
           ></input>
           <input
             className="w-[528px] mx-auto block  mb-[24px] px-3 py-4 rounded-lg font-inter text-customGrey bg-bgInput"
             type="email"
             placeholder="jgardner@gmail.com"
+            onChange={handleOnChange}
+            name="email"
+            value={data.email}
           ></input>
           <input
             className="w-[528px] mx-auto block  mb-[22px] px-3 py-4 rounded-lg text-customGrey bg-bgInput"
             type="password"
             placeholder="*************"
+            onChange={handleOnChange}
+            name="password"
+            value={data.password}
           ></input>
 
           <button className="font-inter w-[528px] border-2 py-[14px] text-2xl leading-6 btn-gradient rounded-lg">
@@ -29,11 +65,13 @@ const signUp = () => {
 
         <p className="mt-8 font-inter text-2xl text-customGrey">
           Already have an account?
-          <span className="text-customBlue"> Log in.</span>
+          <Link href="/login">
+            <span className="text-customBlue"> Log in.</span>
+          </Link>
         </p>
       </div>
     </div>
   );
 };
 
-export default signUp;
+export default signup;
